b81b6dea432ba8f5464e8fac09656cdac4a9c827
Nick Powell <nickpowells@gmail.com>
2017-03-24 11:20:32 -0700

    add functioning submit button to quiz form.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,23 @@ var android = "JAVA/Android track";
 var design = "JAVA/Android track";
 var cee = "C#/.NET";
 
-$ (function()
-  $("#quiz").submit(function(event){
+$(function() {
+  $("#quiz-form").submit(function(event) {
+    var size = $("input:radio[name=size]:checked").val();
+    var focus = $("input:radio[name=focus]:checked").val();
+    var end = $("input:radio[name=end]:checked").val();
+    var design = $("input:radio[name=design]:checked").val();
+    var content = $("input:radio[name=content]:checked").val();
+
+
+    if (size === "true") {
+      $(".output").text(cee);
+    }
+
+    else {
+        $(".output").text(ruby);
+    }
+
+    event.preventDefault();
+  });
 });
