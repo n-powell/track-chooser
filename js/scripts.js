0bb601268cfac2436ab23135d3f7e7dd8d10a2e5
Nick Powell <nickpowells@gmail.com>
2017-03-24 12:15:28 -0700

    add initial styling concept.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
 var ruby = "Ruby/Rails track";
 var drupal = "PHP track";
 var android = "JAVA/Android track";
-var design = "JAVA/Android track";
+var design = "CSS/Design";
 var cee = "C#/.NET";
 
 $(function() {
   $("#quiz-form").submit(function(event) {
     var size = $("input:radio[name=size]:checked").val();
-    var focus = $("input:radio[name=focus]:checked").val();
-    var end = $("input:radio[name=end]:checked").val();
-    var design = $("input:radio[name=design]:checked").val();
-    var content = $("input:radio[name=content]:checked").val();
+    var focus = parseInt($("input:radio[name=focus]:checked").val());
+    var end = parseInt($("input:radio[name=end]:checked").val());
+    var design = parseInt($("input:radio[name=design]:checked").val());
+    var content = parseInt($("input:radio[name=content]:checked").val());
 
 
-    if (size === "true") {
-      $(".output").text(cee);
-    }
+    if (size === "large") {
+      if (focus === 1) {
+        $(".output").text(cee)
+      }else if (focus > 1) {
+          if (end > 1 ) {
+            $(".output").text(ruby)}
+          }if (focus > 1) {
+              if (end === 1) {
+                $(".output").text(design);}
 
-    else {
-        $(".output").text(ruby);
+
+      }
+
+
+
+
+
+    }else {
+        $("output").text(ruby);
     }
 
     event.preventDefault();
